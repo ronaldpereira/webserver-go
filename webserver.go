e3a55e752cd5eccab8da4c0d62889c847bc72d89
Ronald Pereira <ronald.drp11@gmail.com>
2018-02-02 16:58:05 -0200

    Webserver funcional. 90% das vezes imprime um número inteiro entre 0 e 1000000, 10% das vezes imprime uma string aleatória de tamanho aleatório (entre 1 e 200).

--- webserver.go
+++ webserver.go
@@ -8,9 +8,22 @@ import	(
 		)
 
 func randomic (writer http.ResponseWriter, request *http.Request) {
-	rand.Seed(time.Now().UnixNano()) // Random seed based on actual system time
+	rand.Seed(time.Now().UnixNano()) // System time seed
 	number := rand.Intn(100)
-	fmt.Fprintf(writer, "%d\n", number)
+
+	if number < 90 { // 90% chance (from 0 to 89) of printing a random number
+		fmt.Fprintf(writer, "%d", rand.Intn(1000000))
+	} else { // 10% chance (from 90 to 99) of printing a random string
+		var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") // Possible letters to mount the random string
+		strSize := rand.Intn(199) + 1 // Random size from 1 to 200
+		str := make([]rune, strSize)
+
+		for i := range str {
+			str[i] = letters[rand.Intn(len(letters))] // Pick a random letter from letters
+		}
+
+		fmt.Fprintf(writer, "%s", string(str))
+	}
 }
 
 func main ()  {
