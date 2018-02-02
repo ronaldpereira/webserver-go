c361ecbcd420966707bda03b1d6ac5f57030f263
Ronald Pereira <ronald.drp11@gmail.com>
2018-02-02 16:45:55 -0200

    Impressão do número aleatório entre 0 e 99 no navegador

--- webserver.go
+++ webserver.go
@@ -1,6 +1,17 @@
 package main
 
-import "net/http"
+import	(
+		"fmt"
+		"time"
+		"net/http"
+		"math/rand"
+		)
+
+func randomic (writer http.ResponseWriter, request *http.Request) {
+	rand.Seed(time.Now().UnixNano()) // Random seed based on actual system time
+	number := rand.Intn(100)
+	fmt.Fprintf(writer, "%d\n", number)
+}
 
 func main ()  {
 	http.HandleFunc("/randomize", randomic)
